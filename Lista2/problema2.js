67e94cf82265c8820e7a2ae8762d20fcb082555e
acab2 <acab2@cin.ufpe,br>
2018-04-15 16:04:36 -0300

    Quase

--- Lista2/problema2.js
+++ Lista2/problema2.js
@@ -1,24 +1,42 @@
+/***********************************************************
+ Não consegui fazer com que o brush selecionasse corretamente, 
+ porém as interações entre os plots estão funcionando.
+ ***********************************************************/
+
+
+
 var selectGol = false;
 var selectTam = false;
 var selectAzul = false;
+var selectedPoints = [];
+var myBrush = d3.brush();
 
 var dataset = function(){
 	var data = [];
 	var qtdGol = 0;
 	var qtdTam = 0;
 	var qtdAzul = 0;
-	
-	for(i=0;i<1248;i++){
-		if(trips[i].carrier == "Gol"){
-			qtdGol++;
-		}else if(trips[i].carrier == "Tam"){
-			qtdTam++;
-		}else{
-			qtdAzul++;
+	if(selectedPoints.length == 0){
+		for(i=0;i<1248;i++){
+			if(trips[i].carrier == "Gol"){
+				qtdGol++;
+			}else if(trips[i].carrier == "Tam"){
+				qtdTam++;
+			}else{
+				qtdAzul++;
+			}
 		}
-		
+	}else{
+		for(i=0;i<selectedPoints.length;i++){
+			if(selectedPoints[2] == 0){
+				qtdGol++;
+			}else if(selectedPoints[2] == 1){
+				qtdTam++;
+			}else{
+				qtdAzul++;
+			}
+		}
 	}
-	
 	data.push({"carrier":"Gol", "quantity": qtdGol});
 	data.push({"carrier":"Tam", "quantity": qtdTam});
 	data.push({"carrier":"Azul", "quantity": qtdAzul});
@@ -27,27 +45,33 @@ var dataset = function(){
 	
 }
 
-var dataset3 = [];
+var dataset2 = [];
 
 var dataset2f = function(){
 
 	var datePost = "";
     var dateStart = "";
-	dataset3 = [];
-	for(i=0;i<1248;i++){
-		datePost = (trips[i].post).split('/');
-		dateStart = (trips[i].start).split('/');
-		
-		qtdDays = ((new Date(dateStart[2], dateStart[1]-1, dateStart[0])) - (new Date(datePost[2], datePost[1]-1, datePost[0]))) / 86400000;
-		if(trips[i].carrier == "Gol" && selectGol){
-			dataset3.push([qtdDays,trips[i].price]);
+	dataset2 = [];
+	
+	if(selectedPoints.length == 0){
+		for(i=0;i<1248;i++){
+			datePost = (trips[i].post).split('/');
+			dateStart = (trips[i].start).split('/');
+			
+			qtdDays = ((new Date(dateStart[2], dateStart[1]-1, dateStart[0])) - (new Date(datePost[2], datePost[1]-1, datePost[0]))) / 86400000;
+			if(trips[i].carrier == "Gol" && selectGol){
+				dataset2.push([qtdDays,trips[i].price, 0]);
+			}
+			if(trips[i].carrier == "Tam" && selectTam){
+				dataset2.push([qtdDays,trips[i].price, 1]);
+			}
+			if(trips[i].carrier == "Azul" && selectAzul){
+				dataset2.push([qtdDays, trips[i].price, 2]);
+			}
+			
 		}
-		if(trips[i].carrier == "Tam" && selectTam){
-			dataset3.push([qtdDays,trips[i].price]);
-		}
-		if(trips[i].carrier == "Azul" && selectAzul){
-			dataset3.push([qtdDays, trips[i].price]);
-		}
+	}else{
+		dataset2 = selectedPoints;
 		
 	}
 }
@@ -91,9 +115,9 @@ var svg = d3.select("body").append("svg")
       .style("text-anchor", "middle")
       .text("Travels"); 
 	
-	svg.append("g").selectAll(".bar")
-      .data(dataset)
-    .enter().append("rect")
+	var rects = svg.append("g").selectAll(".bar");
+	  
+    var rectGroup = rects.data(dataset).enter().append("rect")
       .attr("class", "bar")
       .attr("x", function(d) { return xScale(d.carrier)-25; })
       .attr("width", 50)
@@ -113,7 +137,7 @@ var svg = d3.select("body").append("svg")
 			
 			updatePlot();
 
-        });;
+        });
 	
 var xScale2 = d3.scaleLinear().domain([0, 400]).range([0, width]);
 var yScale2 = d3.scaleLinear().domain([0, 1800]).range([height, 0]);
@@ -148,15 +172,71 @@ var svg2 = d3.select("body").append("svg")
       .text("Price");  
 	
 	var dotGroup = svg2.append("g").attr("transform", "translate(0," + (-height) + ")");
+	var brushGroup = svg2.append("g").attr("transform", "scale(1,-1)");
+
+	myBrush.on("start",function(){
+		updatePlot();
+	});
+	
+	
+	myBrush.on("brush",function(){
+    selectedPoints = [];
+    var selection = d3.event.selection;
+    //debugger
+		dotGroup.selectAll("circle")
+		.attr("fill",function(d){
+			if(((selection[0][0]) <= (d[0]) && (d[0]) <= selection[1][0] &&
+			 (selection[0][1]) <= (d[1]) && (d[1]) <= (selection[1][1]) )){
+				 
+			 selectedPoints.push([d[0],d[1], d[2]]);
+			 
+			return "red";
+			 }else{
+			 return "steelblue";}
+		});
+		
+	console.log(selectedPoints.length);
+	rectGroup.exit().remove();
+	
+	if(selectedPoints.length == 0){
+		rectGroup.data(dataset)
+		.attr("y", function(d) { return yScale(d.quantity); })
+      .attr("height", function(d) { return height - yScale(d.quantity); })
+	  rectGroup.exit().remove();
+	}else{	
+		rectGroup.data(selectedPoints).attr("y", function(d) { 
+													if(d[2] == 0 && selectGol){return yScale(d[1]);}else
+													if(d[2] == 1 && selectTam){return yScale(d[1]);}else
+													if(d[2] == 2 && selectAzul){return yScale(d[1]);}else{return 0;}
+				})
+			.attr("height", function(d) { return height - yScale(d[1]); });
+			 
+    }  
+	
+	});
+
+	myBrush.on("end",function(){
+    updatePlot();
+	});
+	
+myBrush(brushGroup);
 	
 function updatePlot(){
 	dataset2f();
-	
-	dots = dotGroup.selectAll("circle").data(dataset3);
+	if(selectedPoints.length > 0){
+		
+		rectGroup.data(selectedPoints);
+		dots = dotGroup.selectAll("circle").data(selectedPoints);
+		
+	}else{
+			
+		dots = dotGroup.selectAll("circle").data(dataset2); 	
+	}
 	dots.exit().remove();
 	dots.enter().append("circle")
-      .attr("r", 3)
-      .attr("cx", d=> xScale2(d[0]))
-      .attr("cy", d=> yScale2(d[1]))
-	  .attr("fill", "steelblue");  
+		.attr("r", 3)
+		.attr("cx", d=> xScale2(d[0]))
+		.attr("cy", d=> yScale2(d[1]))
+		.attr("fill", "steelblue");
 }
+
